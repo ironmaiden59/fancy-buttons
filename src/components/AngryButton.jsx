fd8a5fe8011b1cc3b5abf9de4d5ad05ca3a66be0
ralph <ralphdcouto1987@gmail.com>
2023-09-07 21:37:50 -0400

    creating handle click even

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function AngryButton () {
+  const [anger, setAnger] = useState(0);
+  const handleClick = (amount) => {
+    if (anger < 1) {
+      setAnger(anger + amount)
+    } else {
+      setAnger(0);
+    }
+  }
 return (
-  <button className="AngryButton">
-  {/* When the threshold is not reached */}<span>Don't click me too much! </span>
-  {/* When the threshold is not reached */}<span>Rawr!</span>
+  <button style={{backgroundColor: `rgba(255,0,0,${anger})`}} onClick={() => handleClick(0.1)} className="AngryButton">
+  {anger < 1 && <span>Don't click me too much!</span>}
+  {anger > 1 && <span>Rawr!</span>}
 </button>
 );
 }

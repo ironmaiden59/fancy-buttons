5c3fa6d40a5ce41fe670001ef515b56982a6f139
ralph <ralphdcouto1987@gmail.com>
2023-09-08 01:21:35 -0400

    refactoring angrybutton

--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
-function LightSwitchButton () {
-  const [light, setLight] = useState("off");
+function LightSwitchButton ({light, switchLight}) {
+  
+  const handleClick = () => switchLight();
   return (
-    <button className="LightSwitchButton">
+    <button onClick={switchLight} className="LightSwitchButton">
     {light === "on" &&<span><i>💡</i> I'm on!</span>}
     {light === "off" &&<span className="off"><i>💡</i> I'm off!</span>}
   </button>
